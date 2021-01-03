2446439832a0692d8226a842872c5673d5f7b36c
Amna-Nazir <73432041+Amna-Nazir@users.noreply.github.com>
2021-01-04 02:00:09 +0500

    Assignment 4

---
+++ SpeakHello.js
@@ -0,0 +1,13 @@
+(function (window){
+	var speakWord = "Hello";
+
+
+var helloSpeaker={
+	
+ speak: function (name) {
+ return console.log(speakWord + " " + name);
+}
+}
+window.helloSpeaker=helloSpeaker;
+
+}(window));
